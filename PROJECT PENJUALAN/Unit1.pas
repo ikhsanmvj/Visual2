4729f23a14a30da90aea511553c8a89acbbfad48
ikhsanmvj <136589168+ikhsanmvj@users.noreply.github.com>
2024-06-04 22:29:35 +0800

    Add files via upload

---
+++ PROJECT PENJUALAN/Unit1.pas
@@ -0,0 +1,40 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Menus;
+
+type
+  TForm1 = class(TForm)
+    mm1: TMainMenu;
+    FILE1: TMenuItem;
+    LOGIN1: TMenuItem;
+    LOGOUT1: TMenuItem;
+    DATAMASTER1: TMenuItem;
+    KATEGORI1: TMenuItem;
+    SATUAN1: TMenuItem;
+    SUPPLIER1: TMenuItem;
+    KUSTOMER1: TMenuItem;
+    RANSAKSI1: TMenuItem;
+    PENJUALAN1: TMenuItem;
+    PEMBELIAN1: TMenuItem;
+    LAPORAN1: TMenuItem;
+    LAPORANPENJUALAN1: TMenuItem;
+    LAPORANSTOKBARANG1: TMenuItem;
+    LAPORANBARANG1: TMenuItem;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ PROJECT PENJUALAN/Unit2.dfm
@@ -0,0 +1,32 @@
+object Form2: TForm2
+  Left = 176
+  Top = 200
+  Width = 870
+  Height = 500
+  Caption = 'Form2'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object el: TEdit
+    Left = 112
+    Top = 144
+    Width = 121
+    Height = 21
+    TabOrder = 0
+    Text = 'el'
+  end
+  object LOGIN: TButton
+    Left = 112
+    Top = 176
+    Width = 75
+    Height = 25
+    Caption = 'LOGIN'
+    TabOrder = 1
+  end
+end
